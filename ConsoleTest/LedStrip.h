d11ed02f21b5513e603d21ccf27aca9f01b8f4d3
Eric Gunnerson <eric_gunnerson@hotmail.com>
2019-08-25 11:43:38 -0700

    stateful version

--- ConsoleTest/LedStrip.h
+++ ConsoleTest/LedStrip.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#ifdef OldVersion
+
 class LedStrip
 {
 public:
@@ -13,3 +15,48 @@ public:
 		printf("Show: \n");
 	}
 };
+
+#else
+
+class LedColor
+{
+public:
+	int Red;
+	int Green;
+	int Blue;
+
+	LedColor() : LedColor(0, 0, 0)
+	{
+	}
+
+	LedColor(int red, int green, int blue)
+	{
+		Red = red;
+		Green = green;
+		Blue = blue;
+	}
+};
+
+
+class LedStrip
+{
+	LedColor _colors[15];
+
+public:
+	void setColor(int ledNumber, int red, int green, int blue)
+	{
+		_colors[ledNumber] = LedColor(red, green, blue);
+	}
+
+	void show()
+	{
+		printf("Show: ");
+		for (int i = 0; i < 15; i++)
+		{
+			printf("(%d,%d,%d)", _colors[i].Red, _colors[i].Green, _colors[i].Blue);
+		}
+		puts("");
+	}
+};
+
+#endif
