58389a085fbbae3406450c3d58113e335461e0f0
Eric Gunnerson <eric_gunnerson@hotmail.com>
2019-08-24 17:03:23 -0700

    moved FastLED code to new LedStrip class...

---
+++ Arduino/Larson/src/LedStrip.h
@@ -0,0 +1,25 @@
+#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+#include <fastled.h>
+
+#define NUM_LEDS 15
+#define DATA_PIN 3
+
+class LedStrip
+{
+    CRGB _leds[NUM_LEDS];
+
+    public:
+    void setup() {
+        FastLED.addLeds<NEOPIXEL, DATA_PIN>(_leds, NUM_LEDS);
+    }
+
+    void setColor(int ledIndex, int red, int green, int blue)
+    {
+        _leds[ledIndex] = CRGB(red, green, blue);
+    }
+
+    void show()
+    {
+        FastLED.show();
+    }
+};
